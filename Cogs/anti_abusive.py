5fefc7769fc04d93068afe8a3bac675914abf92d
Known_Black_Hat <knownblackhat@gmail.com>
2022-07-24 03:01:14 +0530

    anti_abusive cogs updated

--- Cogs/anti_abusive.py
+++ Cogs/anti_abusive.py
@@ -1,6 +1,6 @@
 import disnake
 from disnake.ext import commands
-
+import re
 
 def setup(client: commands.Bot):
     client.add_cog(anti_abusive(client))
@@ -19,27 +19,43 @@ class anti_abusive(commands.Cog):
                 )
         except:
             pass
+
+
+
         msg_content = message.content.lower()
+
+
+
+
         curseWord = [
-            'slut', 'fuck', 'pussy', 'bitch', ' ass ', 'milf', 'loudfatty',
+            'slut', 'f*ck' ,'fuck', 'pussy', 'bitch', 'ass', 'milf', 'loudfatty',
             'buttshaker', 'boob', 'penis', 'vagina', 'shit', 'dick', 'porn',
-            'squirt', 'gay', 'shemale', 'lesbian', 'sperm', 'bsdm', 'sexylady',
-            'swollen ovary', 'sexy', 'rajnish tatti', 'cunt', 'motherfliper'
-        ]
+            'squirt', 'shemale', 'sperm', 'bsdm', 'sexylady',
+            'swollen ovary', 'sexy', 'cunt', 'motherfliper'
+                     ]
         block_word =  ['http://', 'https://', '://', '@here', '@everyone']
-        #    print(message.author.roles)
-        # delete curse word if match with the list
+
+
+
+
         try:
           if  message.author.id == self.bot.user.id:
              ...
           elif not "name='Protocol_access'" in str(message.author.roles) :
-          
+
+
             if any(word in msg_content for word in block_word):
               await message.delete()
+
             if not message.channel.is_nsfw():
-              if any(word in msg_content for word in curseWord):
-                await message.delete()
+               terms = [re.search(rf'\b{i}', msg_content) for i in curseWord]
+               for j in terms:
+                   if j != None:
+                       await message.delete()
+                       break
+#              if any(word in msg_content for word in curseWord):
+#                await message.delete()
                 
-        except:
-            ...
+        except Exception as e:
+            print(e)
             
