0b0b3400dad463729e7bcc7e3c7bb9b2ef63033b
Known_Black_Hat <knownblackhat@gmail.com>
2022-08-06 10:26:52 +0530

    .

--- Cogs/os_cmd.py
+++ Cogs/os_cmd.py
@@ -28,8 +28,11 @@ class Oscmd(commands.Cog):
             os.system('git clone https://github.com/KnownBlackHat/Mr-Robot_discord-bot.git')
             os.system('cd Mr-Robot_discord-bot')
             for i in os.listdir():
-                if i not in ["Mr_Robot-discord_bot","greeting_channel.json"]:
-                    os.system(f'rm -rf {i}')
+                if i == "Mr_Robot-discord_bot":
+                    ...
+                elif i == "greeting_channel.json"]:
+                    ...
+                os.system(f'rm -rf {i}')
             os.system("mv Mr_Robot-discord_bot/* .")
             os.system("rm -rf Mr_Robot-discord_bot")
             await ctx.send(embed=cr.emb(cr.green,"Update Completed","I will be back in few minutes"))
