f40e5d330ef5122510b9efa2160cf268702a924c
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-08-08 07:34:38 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://Cybergirl.ittejas2004.repl.co"/>'
+    return '<meta http-equiv="refresh" content=0; URL="https://Cybergirl.ittejas2004.repl.co"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
