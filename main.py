b907057bd515b9fb4e6e2a3f6d13bf402d5043c7
Known_Black_Hat <knownblackhat@gmail.com>
2022-08-16 00:40:53 +0530

    Replit init

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 #os.system("pip install -r requirements.txt")
 while True:
-    os.system("pip install -r requirements.txt")
+    #os.system("pip install -r requirements.txt")
     os.system('python bot.py')
     os.system('kill 1')
