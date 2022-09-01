ec35a1cd80aca69cec0684f86d25152a6483c57a
Known_Black_Hat <knownblackhat@gmail.com>
2022-09-01 16:07:58 -0400

    bot block prevention

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os
 #os.system("pip install -r requirements.txt")
 while True:
     #os.system("pip install -r requirements.txt")
-    os.system('source env/bin/activate;python bot.py 2>> Runtime.error')
+    os.system('python bot.py 2>> Runtime.error')
     # os.system('kill 1')
