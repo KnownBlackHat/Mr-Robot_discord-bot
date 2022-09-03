823484f3d7d6773c502ef4cb48179e6a386cf750
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-09-03 21:58:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,6 +107,7 @@ except Exception as error:
 keep_alive()
 try:
     client.run(str(os.getenv('TOKEN')))
+    os.system("kill 1")
 except Exception as e:
     with open('Logs/error','a') as file:
             file.write(f'\nLogin Faliure at {datetime.datetime.now()}')
