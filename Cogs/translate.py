d47098e6cf664440ab0b3d2ff17cc07d60e7f01f
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-09-12 17:04:20 -0400

    Update translate.py

--- Cogs/translate.py
+++ Cogs/translate.py
@@ -3,7 +3,7 @@ from bot import cr
 import os
 os.system("pip install googletrans==3.1.0a0")
 from googletrans import Translator
-
+import googletrans as gt
 
 def setup(client: commands.Bot):
     client.add_cog(translate(client))
@@ -17,8 +17,8 @@ class translate(commands.Cog):
         translator = Translator()
         try:
             translation = translator.translate(thing, dest=lang)
+            await ctx.send(embed = cr.emb(cr.orange,f"Translation To {lang}",translation.text))
         except Exception as e:
             if "invalid destination language" in str(e):
-                await ctx.send(embed = cr.emb(cr.orange,f"Destination Translation Language List (DTLL) :",googletrans.LANGUAGES))
-        await ctx.send(embed = cr.emb(cr.orange,f"Translation To {lang}",translation.text))
+                await ctx.send(embed = cr.emb(cr.orange,f"Destination Translation Language List (DTLL) :",str(gt.LANGUAGES).replace(",","\n")))
       
