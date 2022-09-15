1d95e04118cd3c564741ae8511c9f375cff2c6a8
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-09-15 03:52:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 #os.system("python -m poetry add disnake")
 while True:
-    os.system("pip install -r requirements.txt")
+    os.system("clear")
+    print("[+] Installing Required Modules...")
+    os.system("pip install -r requirements.txt &> pip.log")
+    print("[=] Starting Bot...")
     os.system('python bot.py')
