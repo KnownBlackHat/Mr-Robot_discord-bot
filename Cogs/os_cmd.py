a813e770cb905c070d4f95c2a72e747577e44f1f
KnownBlackHat <knownblackhat@gmail.com>
2022-09-18 06:51:32 -0400

    implemented slash command

--- Cogs/os_cmd.py
+++ Cogs/os_cmd.py
@@ -25,7 +25,7 @@ class Oscmd(commands.Cog):
         await self.bot.change_presence(status=disnake.Status.idle,activity=disnake.Game(name='Update'))
         await ctx.send(embed=cr.emb(cr.green,"Updating..."))
         os.system("rm -rf Mr*")
-        os.system('git clone -b Mr-Robot https://github.com/KnownBlackHat/Mr-Robot_discord-bot.git')
+        os.system('git clone -b Mr_Robot https://github.com/KnownBlackHat/Mr-Robot_discord-bot.git')
         for i in os.listdir():
             if i == "Mr-Robot_discord-bot":
                 ...
