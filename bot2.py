dc2a044f3ea2af0040997a90697b0861c08f44c9
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-10-16 00:32:42 +0530

    Create bot2.py

---
+++ bot2.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import tasks, commands
+from dotenv import load_dotenv
+load_dotenv()
+import os
+client = commands.Bot(
+  command_prefix=':',intents = discord.Intents.all(),
+  self_bot=True
+)
+@client.event
+async def on_ready():
+    print(f'\n [!] Token2 Logged in as {client.user}')
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name='OUR WORLD GETTING HACKED'))
+client.run(os.getenv("TOKEN2"), bot=False)
