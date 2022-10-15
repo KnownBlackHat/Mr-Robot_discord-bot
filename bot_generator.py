23984e62f765c8242d245eea6c712832588fa48d
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-10-16 01:39:48 +0530

    Update bot_generator.py

--- bot_generator.py
+++ bot_generator.py
@@ -1,5 +1,5 @@
 
-# Argument Format: python bot_generator.py <token no.> <change presence syntax>
+# Argument Format: python bot_generator.py <token no.> <status no.> <name> 
 
 import discord
 from discord.ext import tasks, commands
@@ -7,12 +7,25 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 import sys
-client = commands.Bot(
-  command_prefix=':',intents = discord.Intents.all(),
-  self_bot=True
-)
+client = commands.Bot(command_prefix=':',intents = discord.Intents.all(),self_bot=True)
 def get_cmd():
-  return sys.argv[2]
+  if sys.argv[2] == '1':
+    # Setting `Playing Game` status
+    return await bot.change_presence(activity=discord.Game(name=f"sys.argv[3]"))
+
+  elif sys.argv[2] == '2':
+    # Setting `Streaming ` status
+    return await bot.change_presence(activity=discord.Streaming(name=f"sys.argv[3]", url="https://www.twitch.tv/amouranth"))
+
+  elif sys.argv[2] == '3':
+    # Setting `Listening ` status
+    return await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f"sys.argv[3]"))
+
+  elif sys.argv[2] == '4':
+    # Setting `Watching ` status
+    return await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"sys.argv[3]"))
+
+    
 @client.event
 async def on_ready():
     print(f'\n [!] TOKEN{sys.argv[1]} Logged in as {client.user}')
