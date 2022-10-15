621223a78b048a2baceed6463aa29461fafdf36e
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-10-16 00:36:34 +0530

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -10,5 +10,5 @@ client = commands.Bot(
 @client.event
 async def on_ready():
     print(f'\n [!] Token2 Logged in as {client.user}')
-    await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name='OUR WORLD GETTING HACKED'))
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name='Over This Server'))
 client.run(os.getenv("TOKEN2"), bot=False)
