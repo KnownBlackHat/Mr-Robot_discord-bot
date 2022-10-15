9dbbb190a39cffcaac4e3ee45e3cd985a7a7d0e0
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-10-16 00:49:41 +0530

    Update and rename bot1.py to bot_generator.py

--- bot_generator.py
+++ bot_generator.py
@@ -3,12 +3,13 @@ from discord.ext import tasks, commands
 from dotenv import load_dotenv
 load_dotenv()
 import os
+import sys
 client = commands.Bot(
   command_prefix=':',intents = discord.Intents.all(),
   self_bot=True
 )
 @client.event
 async def on_ready():
-    print(f'\n [!] Token1 Logged in as {client.user}')
+    print(f'\n [!] TOKEN{sys.argv[1]} Logged in as {client.user}')
     await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name='OUR WORLD GETTING HACKED'))
-client.run(os.getenv("TOKEN1"), bot=False)
+client.run(os.getenv(f"TOKEN{sys.argv[1]}"), bot=False)
