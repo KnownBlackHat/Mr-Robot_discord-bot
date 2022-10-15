eb78ef3259e351ae2ca0e25f01e507cdccaf3ba0
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-10-16 01:02:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import os
 #os.system("python -m poetry add disnake")
 os.system("pip install -r requirements.txt ")
 os.system("clear")
-#os.system("pip uninstall -y ")
-#os.system("pip uninstall -yr pip_log.txt")
-#os.system("pip install discord.py==1.7.3")
 
-#     print("[=] Starting Bot...")
 os.system('pip freeze > pip_log.txt')
 os.system('python bot1.py &> bot1.log & ')
 os.system('python bot2.py &> bot2.log & ')
