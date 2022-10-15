541984f5fb78308b6d6db4317ae6a2751ae12c4d
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-10-16 02:14:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ os.system("pip install -r requirements.txt ")
 os.system("clear")
 
 os.system('pip freeze > pip_log.txt')
-os.system('python bot_generator.py 1 4 "Our World Getting Hacked" & ')
-os.system('python bot_generator.py 2 4 "Over This Server" & ')
-
+os.system('python bot_generator.py Known_Black_Hat 1 "Our World Getting Hacked" false & ')
+os.system('python bot_generator.py Cyber_Girl 4 "Over This Server" false & ')
+os.system('python bot_generator.py Desus 2 "Join Me" false & ')
 os.system('python bot.py')
