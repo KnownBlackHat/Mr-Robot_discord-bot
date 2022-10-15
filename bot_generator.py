83f5004698682282b75295a685f0b0a5f1ccadb9
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-10-16 00:59:58 +0530

    Update bot_generator.py

--- bot_generator.py
+++ bot_generator.py
@@ -1,3 +1,6 @@
+
+# Argument Format: python bot_generator.py <token no.> <change presence syntax>
+
 import discord
 from discord.ext import tasks, commands
 from dotenv import load_dotenv
@@ -8,8 +11,11 @@ client = commands.Bot(
   command_prefix=':',intents = discord.Intents.all(),
   self_bot=True
 )
+def get_cmd():
+  return sys.argv[2]
 @client.event
 async def on_ready():
     print(f'\n [!] TOKEN{sys.argv[1]} Logged in as {client.user}')
-    await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name='OUR WORLD GETTING HACKED'))
+    await get_cmd()
+#     await client.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name='OUR WORLD GETTING HACKED'))
 client.run(os.getenv(f"TOKEN{sys.argv[1]}"), bot=False)
