ae24dda7ab1779e50d2bd27a4d2befc48f5a540a
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-10-16 01:44:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ os.system("pip install -r requirements.txt ")
 os.system("clear")
 
 os.system('pip freeze > pip_log.txt')
-os.system('python bot1.py &> bot1.log & ')
-os.system('python bot2.py &> bot2.log & ')
+os.system('python bot_generator.py 1 4 "Our World Getting Hacked" & ')
+os.system('python bot_generator.py 2 4 "Over This Server" & ')
+
 os.system('python bot.py')
