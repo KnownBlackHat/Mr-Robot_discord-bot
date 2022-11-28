79749d425dcd68376123ec5c38595adc198de1e2
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-11-28 21:31:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import os
-#os.system("python -m poetry add disnake")
+import requests
+from bs4 import BeautifulSoup
+import random        
+
+
+def proxy_generator():
+  response = requests.get("https://sslproxies.org/")
+  soup = BeautifulSoup(response.content, 'html5lib')
+  proxy = f"http://{random.choice(list(map(lambda x:x[0]+':'+x[1], list(zip(map(lambda x:x.text, soup.findAll('td')[::8]), map(lambda x:x.text, soup.findAll('td')[1::8]))))))}"
+  return proxy
+
+
 os.system("pip install -r requirements.txt ")
 os.system("clear")
 
-os.system('pip freeze > pip_log.txt')
-# os.system('python bot_generator.py Known_Black_Hat 1 "Our World Getting Hacked" false & ')
-# os.system('python bot_generator.py Cyber_Girl 4 "Over This Server" false & ')
-# os.system('python bot_generator.py Desus 2 "Join Us" false & ')
-os.system('python bot.py')
+#os.system('pip freeze > pip_log.txt')
+
+while True:
+  proxy = proxy_generator()
+  os.system('python bot.py')
