a8fadaa6b0c5f1c2d3d143b0a8ce41f7fd2dd634
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2022-11-28 23:34:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import os
-import requests
-from bs4 import BeautifulSoup
-import random        
-
-
-def proxy_generator():
-  response = requests.get("https://sslproxies.org/")
-  soup = BeautifulSoup(response.content, 'html5lib')
-  proxy = f"http://{random.choice(list(map(lambda x:x[0]+':'+x[1], list(zip(map(lambda x:x.text, soup.findAll('td')[::8]), map(lambda x:x.text, soup.findAll('td')[1::8]))))))}"
-  return proxy
-
-
-os.system("pip install -r requirements.txt ")
-os.system("clear")
-
-#os.system('pip freeze > pip_log.txt')
 if __name__=="__main__":
   while True:
     os.system('python bot.py')
+    import os
+    os.system("pip install -r requirements.txt ")
+    os.system("clear")
+else:
+  import requests
+  from bs4 import BeautifulSoup
+  import random        
+
+
+  def proxy_generator():
+    response = requests.get("https://sslproxies.org/")
+    soup = BeautifulSoup(response.content, 'html5lib')
+    proxy = f"http://{random.choice(list(map(lambda x:x[0]+':'+x[1], list(zip(map(lambda x:x.text, soup.findAll('td')[::8]), map(lambda x:x.text, soup.findAll('td')[1::8]))))))}"
+    return proxy
+
