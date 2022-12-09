c3f1af769408748112a6582a2fe91a919badd25f
Known_Black_Hat <knownblackhat@gmail.com>
2022-12-10 02:50:02 +0530

    updated fun.py cog and added amount limit and random result abality

--- Cogs/fun.py
+++ Cogs/fun.py
@@ -47,28 +47,37 @@ def setup(client: commands.Bot):
 class fun(commands.Cog):
     def __init__(self, client):
         self.bot = client
+   
     @commands.is_nsfw()
     @commands.slash_command(name='nsfw',description="Shows You Nsfw Content")
-    async def nsfw(self,ctx,search,amount=1):
+    async def nsfw(self,ctx,search,amount:int=1):
         term=search
+        if amount > 100:
+            raise Exception("Amount Should be <= 100")
+        await ctx.response.defer()
         if ctx.channel.is_nsfw():
             Header =  {'User-Agent' : "Magic Browser"}
             type=['best','top','new','rising','hot']
             choice = random.choice(type)
             # await ctx.send(embed=cr.emb(cr.black,"NSFW Command",f"🔎Searching {term} in {choice} category..."))
             # async with ctx.typing():    
-            URL = f"https://www.reddit.com/r/{term}/{choice}.json?limit={int(amount)}"
+            URL = f"https://www.reddit.com/r/{term}/{choice}.json?limit=1000" #{int(amount)}"
             async with aiohttp.request("GET",URL,headers=Header) as resp:
                 if resp.status == 200:
                     data = await resp.json()
+                    nsfw_list=[]
                     try:
                         for d in data["data"]["children"]:
                             try: 
-                                await ctx.send(d["data"]["url_overridden_by_dest"])
+                                # await ctx.send(d["data"]["url_overridden_by_dest"])
+                                nsfw_list.append(str(d["data"]["url_overridden_by_dest"]))
                             except KeyError:
                                 if d["data"]["thumbnail"].startswith('http'):
-                                    await ctx.send(d["data"]["thumbnail"])
+                                    # await ctx.send(d["data"]["thumbnail"])
+                                    nsfw_list.append(str(d["data"]["thumbnail"]))
                         # await ctx.send(embed=cr.emb(cr.black,"NSFW Command",f"🔎Search Of {term} in {choice} category Completed!"))
+                        for i in range(amount):
+                            await ctx.send(random.choice(nsfw_list))
                     except:
                         await ctx.send(embed=cr.emb(cr.red,"NSFW Command","Try Again Later"))
                 else:
@@ -87,18 +96,19 @@ class fun(commands.Cog):
               meme_pic = data["preview"][-2]
               await ctx.send(meme_pic)
            else:
-              await ctx.send(embed=cr.emb(cr.red,"Meme Command",f"Meme not found!"))
+              await ctx.send(embed=cr.emb(cr.red,"Meme Command",f"Meme API not responding!"),ephemeral=True)
 
     @commands.slash_command(name='nsfw_premium',description="Returns results from nsfw website")
     @commands.is_nsfw()
     async def xxx(self,ctx,search,amount=1):
         term = search
+        if amount > 100:
+            raise Exception("Amount Should be <= 100")
+        await ctx.response.defer(ephemeral=True)
         if ctx.channel.is_nsfw():
           ufrm_term = term
           term = term.replace(" ","+")
           term_url = "https://www.xnxx.com/search/"+str(term)
-          # print(await get(term_url))
-          # async with ctx.typing():
           try:
               p=0
               while True:
